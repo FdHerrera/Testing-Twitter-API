fcbf68c4b3b54138dd64a21a75b1ec0315cb88af
Fd Herrera <soyfdherrera@gmail.com>
2022-01-13 20:56:52 -0300

    Commit inicial

---
+++ src/main/java/com/gordo/pipero/GordoPiperoApplication.java
@@ -0,0 +1,13 @@
+package com.gordo.pipero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GordoPiperoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GordoPiperoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gordo/pipero/GordoPiperoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gordo.pipero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GordoPiperoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
